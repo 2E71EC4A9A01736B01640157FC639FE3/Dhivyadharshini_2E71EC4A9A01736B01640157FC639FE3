62f9369e01ee4522a13d1aa9ef050c9d11f78c18
Dhivyadharshini <144451265+2E71EC4A9A01736B01640157FC639FE3@users.noreply.github.com>
2023-09-28 19:05:12 +0530

    Add files via upload

---
+++ main (11).py
@@ -0,0 +1,14 @@
+def linerSearchProduct(productList, targetProduct):
+  indices = []
+  for index,product in enumerate(productList):
+   if product == targetProduct:
+    indices.append(index)
+
+  return indices
+
+
+# Example usage:
+products = ["shoes","boot","loafer","shoes","sandal","shoes"]
+target = "shoes"
+result = linerSearchProduct(products,target)
+print(result)
